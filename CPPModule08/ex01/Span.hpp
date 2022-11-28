f103b901ddb773889e67ddb1c9a7e3be307368c9
Metehan Erkol <merkol@k2m13s01.42kocaeli.com.tr>
2022-11-28 16:50:57 +0300

    m08

---
+++ CPPModule08/ex01/Span.hpp
@@ -0,0 +1,23 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+class Span
+{
+private:
+	unsigned int n;
+public:
+	Span();
+	Span(unsigned int _n);
+	~Span();
+};
+
+Span::Span()
+{
+}
+
+Span::~Span()
+{
+}
+
+
+#endif
